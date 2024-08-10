da591121c913a11cdaaa06858a34b6deea1e7be8
Andreas Wild <codr7@protonmail.com>
2024-08-10 22:42:17 +0200

    Cleaning.

--- src/Hostr/Domain/Tax.cs
+++
@@ -1,9 +0,0 @@
-using static Hostr.DB.ValueExtensions;
-
-namespace Hostr.Domain;
-
-public static class Tax
-{
-    public static decimal Calculate(Cx cx, DB.Record taxType, DateTime timestamp, decimal netAmount) => 
-        TaxRate.Get(cx, taxType, timestamp) * netAmount;
-}
